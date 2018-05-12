ac531c63eda187793d547c6e3aa7051408b2bba3
dmytro_ohorodnykov <swarty16@gmail.com>
2018-05-12 18:11:36 +0300

    changes merge

--- app/js/dmytro.js
+++ app/js/dmytro.js
@@ -1,48 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    // sellect all fields in tables
-    let tableFields = document.querySelectorAll('td');
-
-
-    // select input for entry values in table
-    let input = document.querySelector('.input__field');
+    // settings for edit rows
+    let edittableRows = document.querySelectorAll('td');
+    edittableRows.forEach( element => {
     
-    // mass for entry it on table row place
-    let pressValues = [];
-
-
-
-    // event on press keys and 
-    input.addEventListener('click', function(){
-        
-        document.addEventListener('keypress', function(e){
-
-            letter = e.key;
-
-            pressValues.push(letter);
-
-            // string value from keyboard 
-            pressValues.join('');
-        })
-    })
-
-
-    
-    // add logic
-    tableFields.forEach( eOuter => {
-        eOuter.addEventListener('click', function(){
-            
-            // remove class froom another element
-            tableFields.forEach( eInner => {
-                eInner.classList.remove('td--active');
-            })
-            
-
-            // create connetion with input field and table row
-            input.value = eOuter.textContent;
-            eOuter.classList.add('td--active');
-            
-        })
+    // set for all row attribute edditable
+        element.setAttribute('contenteditable', "true");
     })
 
 
