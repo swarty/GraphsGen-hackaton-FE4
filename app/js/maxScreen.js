864fce00ef0a1ee1fe85cb8143633ee6d00b0984
rebelspirit <vv.malinovskyi@gmail.com>
2018-05-13 15:21:55 +0300

    add max Screen button

---
+++ app/js/maxScreen.js
@@ -0,0 +1,11 @@
+// max Screen Chart button
+function maxScreenChart() {
+	const maxScreenBtn = document.querySelector('#maxScreen');
+	const maxScreenBlock = document.querySelector('.s_block');
+	
+	maxScreenBtn.addEventListener('click', function () {
+		
+});
+};
+maxScreenChart();
+// end max Screen Chart button
---
+++ dist/js/maxScreen.js
@@ -0,0 +1,9 @@
+// max Screen Chart button
+function maxScreenChart() {
+	const maxScreenBtn = document.querySelector('#maxScreen');
+	const maxScreenBlock = document.querySelector('.s_block');
+
+	maxScreenBtn.addEventListener('click', function () {});
+};
+maxScreenChart();
+// end max Screen Chart button
